e64d1d85710802ce1107e05e9d73cf208f43fbef
kdt4 <masun5737@naver.com>
2024-04-26 17:39:40 +0900

    스트링버퍼까지

---
+++ src/starcraft/ver05/IStarCraft.java
@@ -0,0 +1,11 @@
+package starcraft.ver05;
+
+public interface IStarCraft {
+
+	
+	public abstract void a(); 
+//	{
+//		System.out.println(this.name + " 이 " + z.getName() + " 을 공격합니다.");
+//		z.beAttacked(this.power);
+//	}
+}
